884e7ba665f7e18c8cdae0332cb90c0fa93ee75b
Hugo Veloso <hugopcveloso@gmail.com>
2020-03-25 14:21:59 +0000

    changes

--- app/javascript/components/dropdown_replies.js
+++ app/javascript/components/dropdown_replies.js
@@ -1,33 +1,8 @@
 
 
-  /*collapsable section*/
-  /* para cada secção collapsablle*/
-  /* seleccionar e encontrar label. e filtrar pela primeira.*/
-  /* seleccionar */
-
-
 
     const dropDownReplies = () => {
 
-/*    const coll = document.querySelectorAll('.collapsible-btn');
-    const contentFunction = (contentComment) => {
-      if (contentComment.style.display === "block") {
-        contentComment.style.display = "none";
-      } else {
-        contentComment.style.display = "block";
-      }
-    }
-    const collapsibleFunction = (button) => {
-        button.addEventListener("click", function() {
-        button.classList.toggle("active");
-        const content = document.querySelectorAll('.collapsible-content');
-          content.forEach(contentFunction);
-      });
-    }
-
-
-    coll.forEach(collapsibleFunction);
-    }*/
 
   const collapsingThread = () => {
     const collapsableThread = document.querySelector(".collapsible-content");
@@ -45,19 +20,4 @@
 }
 }
 export { dropDownReplies };
-/*const filterDashboard = () => {
-  var x = document.getElementById("pending-cards");
-  var pieActive = document.querySelector(".stats-card-cool");
-  pieActive.addEventListener('click', function() {
-    if (!pieActive.classList.contains('clicked')) {
-      pieActive.classList.toggle('box-shadow');
-      x.style.display = "none";
-      pieActive.classList.add("clicked");
-    } else {
-      x.style.display = "block";
-      pieActive.classList.remove("clicked");
-      pieActive.classList.remove('box-shadow');
-    }
-  });
-}
-*/
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,8 @@ import "bootstrap";
 import { dropDownReplies } from '../components/dropdown_replies';
 
 
-const commentsReply = document.querySelector('.collapsible-btn');
+/*const commentsReply = document.querySelector('.collapsible-btn');
 if (commentsReply) {
   dropDownReplies();
 };
-
+*/
