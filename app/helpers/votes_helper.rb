357b7400cd2f0519df2d9beb4e0b391da180965c
Hugo Veloso <hugopcveloso@gmail.com>
2020-04-22 19:47:21 +0100

    fixed issues with upvotes

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -1,14 +1,11 @@
 module VotesHelper
-    def is_upvoted post
-        if user_signed_in?
-            current_user.upvoted_post_ids.include? post.id ? "active" : ""            
-        end
-    end
+	
+	def is_upvoted post
+		user_signed_in? && current_user.upvoted_post_ids.include?(post.id) ? "active" : ""            
+	end
 
-    def is_downvoted post
-        if user_signed_in?
-            current_user.downvoted_post_ids.include? post.id ? "active" : ""
-        end
-    end
+	def is_downvoted post
+		user_signed_in? && current_user.downvoted_post_ids.include?(post.id) ? "active" : ""   
+	end
 
 end
